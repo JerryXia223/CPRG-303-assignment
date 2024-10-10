976f85424880eca4b5e951d4b95f36ad8bdf8e3d
JerryXia223 <jerry.xia@edu.sait.ca>
2024-10-10 14:19:02 -0600

    todo

---
+++ component/App.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const app = () => {
+  return (
+    <View>
+      <todolist/>
+      <todoform/>
+    </View>
+  )
+}
+
+export default app
